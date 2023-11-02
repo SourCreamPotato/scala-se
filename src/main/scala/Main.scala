270c6b18c6359f5bedec2d6e6748fe4fc44dddbd
dio <wassmus.felix@gmail.com>
2023-11-02 17:23:10 +0100

    functionality: implemented pretty and evaluate

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,150 @@
+import javax.print.MultiDoc
 object Main 
 {
     
-    enum WeekDay {
-      case Mon,Tue,Wed,Thu,Fri,Sat,Sun
+    enum ArithmeticExpression() 
+    {
+      case Num(i: Int)
+      case Minus(exp : ArithmeticExpression)
+      case Plus(exp1 : ArithmeticExpression , exp2 : ArithmeticExpression)
+      case Mult(exp1: ArithmeticExpression , exp2: ArithmeticExpression)
+      case Div(exp1: ArithmeticExpression , exp2: ArithmeticExpression)
+      case Pow(exp1: ArithmeticExpression , exp2: ArithmeticExpression)
     }
+
+
+    object ArithmeticExpression{
+
+      def evaluate(expression : ArithmeticExpression) : Double = 
+      {
+      
+      //if Num
+        if (expression.productPrefix == ArithmeticExpression.Num.toString())
+        {
+          return extractDouble(expression.productElement(0)).get
+        }
+
+      //if Minus
+        if (expression.productPrefix == ArithmeticExpression.Minus.toString())
+        {
+          expression.productElement(0)
+          return (-1) * ArithmeticExpression.evaluate(expression.productElement(0).asInstanceOf[ArithmeticExpression])
+        }
+
+      //if Plus
+        if (expression.productPrefix == ArithmeticExpression.Plus.toString())
+        {
+          
+          
+          return 
+            //left
+            ArithmeticExpression.evaluate(expression.productElement(0).asInstanceOf[ArithmeticExpression]) 
+            +
+            ArithmeticExpression.evaluate(expression.productElement(1).asInstanceOf[ArithmeticExpression])
+            //right
+          
+          }
+
+        if (expression.productPrefix == ArithmeticExpression.Mult.toString())
+        {
+           return 
+            //left
+            ArithmeticExpression.evaluate(expression.productElement(0).asInstanceOf[ArithmeticExpression]) 
+            *
+            ArithmeticExpression.evaluate(expression.productElement(1).asInstanceOf[ArithmeticExpression])
+            //right         
+        }
+
+
+        if (expression.productPrefix == ArithmeticExpression.Div.toString())
+        {
+           return 
+            //left
+            ArithmeticExpression.evaluate(expression.productElement(0).asInstanceOf[ArithmeticExpression]) 
+            /
+            ArithmeticExpression.evaluate(expression.productElement(1).asInstanceOf[ArithmeticExpression])
+            //right         
+        }
+
+        
+        return 1
+      }
+
+      def pretty(expression : ArithmeticExpression) : String = 
+      {
+        
+       
+      //if Num
+        if (expression.productPrefix == ArithmeticExpression.Num.toString())
+        {
+          return  "" + extractDouble(expression.productElement(0)).get.toString() + ""
+        }
+
+      //if Minus
+        if (expression.productPrefix == ArithmeticExpression.Minus.toString())
+        {
+          expression.productElement(0)
+          return "(-"+ ArithmeticExpression.pretty(expression.productElement(0).asInstanceOf[ArithmeticExpression]).toString() + ")"
+        }
+
+      //if Plus
+        if (expression.productPrefix == ArithmeticExpression.Plus.toString())
+        {
+          
+          
+          return 
+            //left
+            "(" + ArithmeticExpression.pretty(expression.productElement(0).asInstanceOf[ArithmeticExpression]).toString() + ")"
+            + "+" +
+            "("+ ArithmeticExpression.pretty(expression.productElement(1).asInstanceOf[ArithmeticExpression]).toString() + ")"
+            //right
+          
+          }
+
+        if (expression.productPrefix == ArithmeticExpression.Mult.toString())
+        {
+          return 
+            //left
+            "(" + ArithmeticExpression.pretty(expression.productElement(0).asInstanceOf[ArithmeticExpression]).toString() + ")"
+            + "*" +
+            "("+ ArithmeticExpression.pretty(expression.productElement(1).asInstanceOf[ArithmeticExpression]).toString() + ")"
+            // right
+          
+ 
+        }
+
+
+        if (expression.productPrefix == ArithmeticExpression.Div.toString())
+        {
+          return ""+
+          //left
+          "(" + ArithmeticExpression.pretty(expression.productElement(0).asInstanceOf[ArithmeticExpression]).toString() + ")"
+          + "/" +
+          "("+ ArithmeticExpression.pretty(expression.productElement(1).asInstanceOf[ArithmeticExpression]).toString() + ")"
+          //right
+        }
+
+        
+       
+        
+          return "result"
+      }
+
+
+    }
+
     
+// from https://stackoverflow.com/questions/42433234/how-to-convert-any-number-to-double
+    def extractDouble(expectedNumber: Any): Option[Double] = expectedNumber match 
+    {
+      case i: Int => Some(i.toDouble)
+      case l: Long => Some(l.toDouble)
+      case d: Double => Some(d)
+      case _ => None
+    }
+
     def main(args: Array[String]): Unit =
     {
-        println("Bomba")
-        println(WeekDay.Mon)
+
     }
 }
